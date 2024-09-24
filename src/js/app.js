91b1a7eae26a38a1502bdb07f9a44d8dd1f49e3c
HXHdevs <jordisonnicolas@gmail.com>
2024-09-23 23:17:59 -0600

    Ajustes para DarkMode

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,25 @@ document.addEventListener('DOMContentLoaded',function(){
 });
 
 function darkMode(){
+    
+    const prefiereDarkMode = window.matchMedia('(prefer-color-scheme:dark)');
+
+    // console.log(prefiereDarkMode.matches);
+
+    if (prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode')
+    } else {
+        document.body.classList.remove('dark-mode')
+    }
+
+    prefiereDarkMode.addEventListener('change', function(){
+        if (prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode')
+        } else {
+            document.body.classList.remove('dark-mode')
+        }   
+    });
+
     const botonDarkMode = document.querySelector('.dark-mode-boton');
     botonDarkMode.addEventListener('click', function(){
         document.body.classList.toggle('dark-mode');
