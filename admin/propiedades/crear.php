f2c818fa5a99cf5b83ecb88beb693533fb89d0a9
HXHdevs <jordisonnicolas@gmail.com>
2024-10-22 00:37:19 -0600

    Propiedades y validaciones para el manejo de imagenes

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -26,6 +26,9 @@
         $vendedorId = mysqli_real_escape_string($db, $_POST['vendedor']);
         $creado = date('Y/m/d');
         
+        $imagen = $_FILES['imagen'];
+
+
         if (!$titulo) {
             $errores[] = "Debes añadir un titulo";
         }        
@@ -47,10 +50,23 @@
         if (!$vendedorId) {
             $errores[] = "Elige vendedor";
         }
+        if (!$imagen['name'] || $imagen['error']) {
+            $errores[] = "La imagen es obligatoria vendedor";
+        }
         
+        $medida = 1000 * 100;
+
+        if ($imagen['size']>$medida) {
+            $errores[]='La imagen es muy pesada';
+        }
+        // exit;
         
         echo "<pre>";
         var_dump($_POST);
+        echo "</pre>";
+        
+        echo "<pre>";
+        var_dump($_FILES);
         echo "</pre>";
 
         if (empty($errores)) {
@@ -83,7 +99,7 @@
             </div>
         <?php endforeach; ?>
 
-        <form class="formulario" method="POST" action="">
+        <form class="formulario" method="POST" action="/bienesraices/admin/propiedades/crear.php" enctype="multipart/form-data">
             <fieldset>
                 <legend>Informacion general</legend>
                     <label for="titulo">Titulo:</label>
@@ -93,7 +109,7 @@
                     <input type="number" id="precio" name="precio" placeholder="Precio Propiedad" value="<?php echo $precio; ?>">
                     
                     <label for="imagen">Imagen:</label>
-                    <input type="file" id="imagen" accept="image/jpeg, image/png" >
+                    <input type="file" id="imagen" accept="image/jpeg, image/png" name="imagen">
 
                     <label for="descripcion">Descripcion:</label>
                     <textarea id="descripcion" name="descripcion"><?php echo $descripcion; ?></textarea>
