1b5a2bf859da8bfe37b758a4e29ad75282eb8883
HXHdevs <jordisonnicolas@gmail.com>
2024-10-25 00:20:50 -0600

    Creando tabla para mostrar el listado de la tabla propiedades

--- admin/propiedades/index.php
+++ admin/propiedades/index.php
@@ -13,6 +13,31 @@
         <?php endif; ?>
 
         <a href="/bienesraices/admin/propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa campirana</td>
+                    <td><img src="../../imagenes/068046074189c19d8379128a8af44923.jpg" class="imagen-tabla"></td>
+                    <td>$1,200,000.00</td>
+                    <td>
+                        <a href="" class="boton-rojo-block">Eliminar</a>
+                        <a href="" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 
 <?php
