3aded740d395c4fdbad5372b516ba686178c56ca
HXHdevs <jordisonnicolas@gmail.com>
2024-11-13 23:48:15 -0600

    Cerrando sesion

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -1,3 +1,11 @@
+<?php 
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+
+    $auth = $_SESSION['login'] ?? null;
+    var_dump($auth);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -26,6 +34,9 @@
                         <a href="anuncios.php">Anuncios</a>
                         <a href="blog.php">Blog</a>
                         <a href="contacto.php">Contacto</a>
+                        <?php if ($auth): ?>
+                            <a href="/cerrar-sesion.php">Cerrar sesion</a>
+                        <?php endif; ?>
                     </nav>
                 </div>
             
