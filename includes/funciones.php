8f09a2d5e99d02f38ba975e172d4e8dcd0ebef45
HXHdevs <jordisonnicolas@gmail.com>
2024-11-28 23:42:47 -0600

    Creando el autoload con composer a nuestro proyecto para usar namespaces

--- includes/funciones.php
+++ includes/funciones.php
@@ -1,6 +1,8 @@
 <?php
 
-require 'app.php';
+
+define('TEMPLATES_URL', __DIR__ . '/templates');
+define('FUNCIONAES_URL', __DIR__. '/funciones.php');
 
 function incluirTemplate(string $nombre,bool $inicio = false){
     include  TEMPLATES_URL . "/${nombre}.php";
